7f72842685059dec0be8be8ed8495d6a80475d42
Mersopolis <mersopolis@gmail.com>
2023-03-17 16:11:35 -0600

    Change schema to Match Criteria

--- db/schema.sql
+++ db/schema.sql
@@ -5,22 +5,28 @@ USE organization_db;
 
 CREATE TABLE departments (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(50) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    role_name VARCHAR(100),
+    title VARCHAR(30),
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    employee_role VARCHAR(50),
-    FOREIGN KEY (employee_role)
-    REFERENCES roles(role_name)
-    employee_department VARCHAR(50),
-    FOREIGN KEY (employee_department)
-    REFERENCES departments(department_name)
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(id)
     ON DELETE SET NULL
 );
